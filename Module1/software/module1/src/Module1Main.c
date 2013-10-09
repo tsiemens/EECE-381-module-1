ed5db25ab7d14efeb9ade2d8ac6b9cabd9509db6
Wesley Tsai <tsai.wesley@gmail.com>
2013-10-08 23:58:06 -0700

    Merge to Menu from master

---
+++ Module1/software/module1/src/Module1Main.c
@@ -0,0 +1,71 @@
+/*
+ * Module1Main.c
+ *
+ *  Created on: 2013-09-27
+ */
+
+#include <stdio.h>
+#include "system.h"
+#include "io.h"
+#include "util/Timer.h"
+#include "io/PS2Keyboard.h"
+#include "io/SDCard.h"
+#include "io/Audio.h"
+#include "video/VideoHandler.h"
+#include "util/ArrayPtr.h"
+#include "sprite/ImgSprite.h"
+#include "sprite/SpriteParser.h"
+#include "game/GameStateMachine.h"
+
+// Approx time per loop for 60 Hz
+#define MAIN_LOOP_MIN_TIME_MS 16
+
+int main()
+{
+
+
+	// INITIALISATION
+	int hasQuit = 0;
+
+	// Timestamp timer setup
+	Timer* loopTimer = Timer_init(Timer_alloc(), MAIN_LOOP_MIN_TIME_MS);
+
+	// Init LEDs
+	unsigned char ledVals = 0;
+	IOWR_8DIRECT(LEDS_BASE, 0, ledVals);
+
+	// TODO other initialisation
+
+	// Video and Character handler init
+	VideoHandlerInit();
+
+	// ImgSprite stuff
+
+	GameStateMachine* stateMachine = GameStateMachine_init(GameStateMachine_alloc(), PS2Keyboard_alloc_init());
+
+	char debugFreqStr[10];
+
+	// MAIN PROGRAM LOOP
+	while (hasQuit == 0) {
+		Timer_start(loopTimer);
+
+		// Debug lights (they increment each frame)
+		ledVals++;
+		IOWR_8DIRECT(LEDS_BASE, 0, ledVals);
+
+		// TODO insert game logic here
+		GameStateMachine_performFrameLogic(stateMachine);
+		// Sleep if finished logic within frame interval
+		while (Timer_isDone(loopTimer) == 0) {
+		}
+
+		// Swap buffers and clear background buffer
+
+		display();
+
+		sprintf(debugFreqStr, "FPS:%2.1f", 1000/Timer_timeElapsed(loopTimer));
+		printString(debugFreqStr, 70, 0);
+	}
+
+	return 0;
+}
